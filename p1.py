f00d0b9fde7101ebe428dc12883348765f49f12d
kkreddyk <kkreddy.k@gmail.com>
2022-04-15 20:16:36 +0100

    print statements

--- p1.py
+++ p1.py
@@ -1,1 +1,21 @@
 # Test Python program
+
+print("Printing...",end="")
+print("2nd line",end="\n")
+print("3rd line")
+
+v1 = "value-1"
+v2="value-2"
+v3="value-3"
+
+print(v1,v2,v3)
+print(v1+v2+v3)
+print("-----------------")
+path="/Users/keerthikaliki/PycharmProjects/py1"
+filenam="sampledata.txt"
+f = open(filenam,"rt")
+print(f.read(10))
+print("-----------------")
+print(f.read())
+print("-----------------")
+print("-----------------")
